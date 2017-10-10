896637585d5cf9a0bf45adad8af45d08459b60ec
PythEch <pythech.tr@gmail.com>
2017-10-10 13:18:11 +0200

    dirty fix for crash on OS X

--- a/Program.cs
+++ a/Program.cs
@@ -9,10 +9,10 @@ namespace a
 
         static void Main(string[] args)
         {
-            IPHostEntry heserver = Dns.GetHostEntry(Dns.GetHostName());
-            foreach (IPAddress curAdd in heserver.AddressList)
-            {
-                Server server = new Server(curAdd, 5000);
+            //IPHostEntry heserver = Dns.GetHostEntry(Dns.GetHostName());
+            //foreach (IPAddress curAdd in heserver.AddressList)
+            //{
+            Server server = new Server(IPAddress.Loopback, 5000);
                 server.Start();
                 Console.WriteLine("Press q to exit");
                 while (true)
@@ -32,7 +32,7 @@ namespace a
                         //who cares ?
                     }
                 }
-            }
+            //}
 
         }
 
