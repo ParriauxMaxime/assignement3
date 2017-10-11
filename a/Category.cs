500a509e0c4107fc07ce23dcd3ca2668cf49c7d3
PythEch <pythech.tr@gmail.com>
2017-10-11 14:37:03 +0200

    Implement Read Request

---
+++ a/Category.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace a
+{
+    class Category
+    {
+        [JsonProperty("cid")]
+        public int Id { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        public static List<Category> Data = new List<Category>
+        {
+            new Category() { Id = 1, Name = "Beverages"},
+            new Category() { Id = 2, Name = "Condiments"},
+            new Category() { Id = 3, Name = "Confections"}
+        };
+
+    }
+
+}
--- a/Server.cs
+++ a/Server.cs
@@ -2,18 +2,22 @@ using System;
 using System.Net.Sockets;
 using System.Net;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+using Newtonsoft.Json.Linq;
 
 namespace a
 {
     class Server
     {
-        private TcpListener _tcpListener;
-        private List<Thread> _threadList;
-        private List<Client> _clientList;
         public Int32 port { get; }
-        private IPAddress address { get; }
-        private Thread _mainThread;
+        public IPAddress address { get; }
+
+        private TcpListener _tcpListener;
         private bool _isRunning = false;
 
         public Server(IPAddress address, Int32 port)
@@ -22,72 +26,178 @@ namespace a
             this.port = port;
             this.address = address;
             _tcpListener = new TcpListener(address, port);
-            _tcpListener.Start();
-            _threadList = new List<Thread>();
-            _clientList = new List<Client>();
-            _mainThread = new Thread(runServer);
         }
 
         public void Start()
         {
             this._isRunning = true;
-            _mainThread.Start();
-        }
-        protected void runServer()
-        {
-            Console.WriteLine("Server started. Waiting for connection.");
-            Thread thread = null;
-            while (this._isRunning)
+
+            try
             {
-                if (_tcpListener.Pending())
-                {
-                    thread = new Thread(acceptConnection);
-                    _threadList.Add(thread);
-                    thread.Start(_tcpListener.AcceptTcpClient());
-                }
+                _tcpListener.Start();
             }
+            catch
+            {
+                Console.WriteLine("Could not start TCP listener!");
+                return;
+            }
+
+            // FIXME: use cancellationtoken to stop
+            Task.Factory.StartNew(() =>
+            {
+                while (this._isRunning)
+                {
+                    var client = _tcpListener.AcceptTcpClient();
+                    Console.WriteLine("Connected");
+
+                    var thread = new Thread(handleRequest);
+                    thread.Start(client);
+                }
+            }, TaskCreationOptions.LongRunning);
+
+            Console.WriteLine("anan");
         }
 
-        protected void acceptConnection(object client)
+        void handleRequest(object clientObj)
         {
-            Console.WriteLine("Accepted Connection");
-            Client c = new Client((TcpClient)client);
-            _clientList.Add(c);
-            Console.WriteLine("Successful connection of client n°{0}.", c.Index);
-            Console.WriteLine(c.Read());
-            c.Write("l");
-            Thread.Sleep(5000);
-            c.Write("Hello world !");
-            this.killConnection(c);
-        }
+            if (clientObj is null) return;
+            if (!(clientObj is TcpClient client)) return;
 
-        protected void killConnection(Client cl)
-        {
-            int number = cl.Index;
-            cl.Close();
-            _clientList.Remove(cl);
-            Console.WriteLine("Client n°{0} disconnected.", number);
+            try
+            {
+                string requestString = Read(client);
+                dynamic json = JsonConvert.DeserializeObject<dynamic>(requestString);
+
+                Response response = new Response();
+
+                if (json.method == "read") {
+                    string path = json.path;
+                    string end = Path.GetFileName(path);
+
+
+                    if (end == "categories")
+                    {
+                        response.Status = Response.StatusCode.Ok;
+                        response.Body = Category.Data;
+
+                    } 
+                    else if (int.TryParse(end, out var id))
+                    {
+                        response.Status = Response.StatusCode.Ok;
+                        var cat = Category.Data.Find(x => x.Id == id);
+                        response.Body = cat;
+                    }
+                }
+
+                Send(client, response.ToJson());
+
+                client.GetStream().Close();
+                client.Dispose();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                //throw;
+            }
         }
 
         public void Stop()
         {
             this._isRunning = false;
-            try
-            {
-                foreach (Client t in _clientList)
-                {
-                    this.killConnection(t);
-                }
-                foreach (Thread t in _threadList)
-                {
-                    t.Join(0);
-                }
-            }
-            catch (Exception) {
-                //Who cares ?
-            }
-            _threadList.Clear();
-            _mainThread.Join(0);
         }
+
+        static void Send(NetworkStream strm, string data)
+        {
+            var response = Encoding.UTF8.GetBytes(data);
+            strm.Write(response, 0, response.Length);
+        }
+
+        static string Read(NetworkStream strm, int size)
+        {
+            byte[] buffer = new byte[size];
+            var bytesRead = strm.Read(buffer, 0, buffer.Length);
+            var request = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            return request;
+        }
+
+        static string Read(TcpClient client)
+        {
+            return Read(client.GetStream(), client.ReceiveBufferSize);
+        }
+
+        static void Send(TcpClient client, string data)
+        {
+            Send(client.GetStream(), data);
+        }
+    }
+
+    class Request
+    {
+        public string Method { get; set; }
+        public string Path { get; set; }
+        public string Date { get; set; }
+        public string Body { get; set; }
+    }
+
+    class Response
+    {
+        public enum StatusCode
+        {
+            Ok = 1,
+            Created,
+            Updated,
+            BadRequest,
+            NotFound,
+            Error
+        };
+
+        public List<string> Reasons = new List<string>();
+        private StatusCode _status;
+
+        public StatusCode Status
+        {
+            get { return _status; }
+            set
+            {
+                _status = value;
+                switch (value)
+                {
+                    case StatusCode.Ok:
+                        Reasons.Add("Ok");
+                        break;
+                    case StatusCode.Created:
+                        Reasons.Add("Created");
+                        break;
+                    case StatusCode.Updated:
+                        Reasons.Add("Updated");
+                        break;
+                    case StatusCode.NotFound:
+                        Reasons.Add("Not Found");
+                        break;
+                    case StatusCode.Error:
+                        Reasons.Add("Error");
+                        break;
+                    case StatusCode.BadRequest:
+                        break;
+                }
+            }
+        }
+        
+
+        public object Body;
+
+        public string ToJson()
+        {
+            string statusString = Status.ToString() + " ";
+            statusString = string.Join(", ", Reasons.ToArray());
+
+            var response = new
+            {
+                status = statusString,
+                body = JsonConvert.SerializeObject(Body)
+            };
+            return JsonConvert.SerializeObject(response);
+        }
+
     }
 }
